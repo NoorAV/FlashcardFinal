343f62ee790d04c6729dc6a2b79af4e4669e5f26
Noor Amir Virani <noorav@noors-macbook-pro.local>
2018-11-02 19:46:15 -0500

    Initial Commit

---
+++ Flashcard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcard
+//
+//  Created by Noor Amir Virani on 11/2/18.
+//  Copyright © 2018 Noor Amir Virani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
